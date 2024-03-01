7a3e14b5ecba1d93a910a1d53f137c962995667c
pjoterm606@gmail.com <p-majchrzak>
2024-03-01 10:17:33 +0100

    Dodano klasy oraz klucz api.

--- Tlumacz/Tlumacz/StronaGlowna.xaml.cs
+++ Tlumacz/Tlumacz/StronaGlowna.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,22 @@ namespace Tlumacz
         public StronaGlowna()
         {
             InitializeComponent();
+        }
+        private const string KluczApi = "AIzaSyBEAMc2DxP4MFiXFyRIWUuzkYy_5RXoki4";
+        public class OdpowiedzTlumaczaGoogle
+        {
+            [JsonProperty("data")]
+            public DaneTlumaczenia Dane { get; set; }
+        }
+        public class DaneTlumaczenia
+        {
+            [JsonProperty("translations")]
+            public Tlumaczenie[] Tlumaczenia { get; set; }
+        }
+        public class Tlumaczenie
+        {
+            [JsonProperty("translatedText")]
+            public string PrzetlumaczonyTekst { get; set; }
         }
     }
 }
